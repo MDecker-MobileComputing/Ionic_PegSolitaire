817d7c67b23db895b3f07729e69aaa806131e56d
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-06-30 11:29:42 +0200

    2dim-Array wird in Form von Zahlenwerten in Grid angezeigt

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+/** Custom Enum für SpielFeldStatus. */
+enum Sfs {
+
+    /** Zellen im GridLayout außerhalb des Rands haben diesen Zustand. */
+    KEIN_FELD,
+
+    /** Spielfeld hat keinen Spielstein */
+    LEER,
+
+    /** Spielfeld hat einen Spielstein */
+    BESETZT
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  /** Zweidimensionaler Array mit Vorlage für initialer Spielfieldzustand. */
+  private readonly SPIELFELD_VORLAGE : Sfs[][] =
+  [
+      [ Sfs.KEIN_FELD, Sfs.KEIN_FELD, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.KEIN_FELD, Sfs.KEIN_FELD ],
+      [ Sfs.KEIN_FELD, Sfs.KEIN_FELD, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.KEIN_FELD, Sfs.KEIN_FELD ],
+      [ Sfs.BESETZT  , Sfs.BESETZT  , Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT  , Sfs.BESETZT   ],
+      [ Sfs.BESETZT  , Sfs.BESETZT  , Sfs.BESETZT, Sfs.LEER   , Sfs.BESETZT, Sfs.BESETZT  , Sfs.BESETZT   ],
+      [ Sfs.BESETZT  , Sfs.BESETZT  , Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT  , Sfs.BESETZT   ],
+      [ Sfs.KEIN_FELD, Sfs.KEIN_FELD, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.KEIN_FELD, Sfs.KEIN_FELD ],
+      [ Sfs.KEIN_FELD, Sfs.KEIN_FELD, Sfs.BESETZT, Sfs.BESETZT, Sfs.BESETZT, Sfs.KEIN_FELD, Sfs.KEIN_FELD ]
+  ];
+
+  /** Zweidimensionaler Array mit Vorlage für initialer Spielfieldzustand. */
+  public spielfeldArray : Sfs[][] = [[]];
+
+  constructor() {
+
+    this.initialisiereSpielfeld();
+  }
+
+  /**
+   * Spielfeld in Ausgangszustand versetzen. Kopiert `SPIELFELD_VORLAGE` in`spielfeldArray`.
+   */
+  private initialisiereSpielfeld() {
+
+    this.spielfeldArray = new Array();
+
+    for (let zeile = 0; zeile < this.SPIELFELD_VORLAGE.length; zeile++) {
+
+      this.spielfeldArray[zeile] = new Array();
+
+      for (let spalte = 0; spalte < this.SPIELFELD_VORLAGE[zeile].length; spalte++) {
+
+        this.spielfeldArray[zeile][spalte] = this.SPIELFELD_VORLAGE[zeile][spalte];
+      }
+    }
+  }
+
+}
